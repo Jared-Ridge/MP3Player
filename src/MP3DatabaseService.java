057968e89ba7ed5beaeb24dd2197b39cc2b82f92
Jared Ridge <jridge.63177@farnborough.ac.uk>
2017-11-10 14:33:06 +0000

    Fixed errors - Added DatabaseConnection class (Fixed error w/ last Commit)
    Signed-off-by: Jared Ridge <jridge.63177@farnborough.ac.uk>

---
+++ src/MP3DatabaseService.java
@@ -0,0 +1,13 @@
+package Model;
+
+import java.io.Console;
+import java.util.List;
+
+class MP3DatabaseService {
+
+    public static void selectAll(List<Console> targetList, DatabaseConnection database) { }
+    public static Console selectById(int id, DatabaseConnection database) { }
+    public static void save(Console console, DatabaseConnection database) { }	// insert & update
+    public static void deleteById(int id, DatabaseConnection database) { }
+
+}
